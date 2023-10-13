01579914cef208d87d43831f01bdc297d86a35c5
Alphar <swamhtet129@gmail.com>
2023-10-13 19:43:07 +0630

    deploymenet fixed 1

--- src/app/components/Card.tsx
+++ src/app/components/Card.tsx
@@ -1,20 +1,23 @@
+import Link from "next/link";
+import Image from "next/image";
+
 export default function Card(){
     return (
         <div className="max-w-sm bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700">
-            <a href="#">
-                <img className="rounded-t-lg" src="/docs/images/blog/image-1.jpg" alt="" />
-            </a>
+            <Link href="#">
+                <Image className="rounded-t-lg" src="/docs/images/blog/image-1.jpg" alt="" />
+            </Link>
             <div className="p-5">
-                <a href="#">
+                <Link href="#">
                     <h5 className="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">
                         Noteworthy technology acquisitions 2021
                     </h5>
-                </a>
+                </Link>
                 <p className="mb-3 font-normal text-gray-700 dark:text-gray-400">
                     Here are the biggest enterprise technology acquisitions of 2021 so far, in
                     reverse chronological order.
                 </p>
-                <a
+                <Link
                     href="#"
                     className="inline-flex items-center px-3 py-2 text-sm font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
                 >
@@ -34,7 +37,7 @@ export default function Card(){
                             d="M1 5h12m0 0L9 1m4 4L9 9"
                         />
                     </svg>
-                </a>
+                </Link>
             </div>
         </div>
 
--- src/app/components/Intro.tsx
+++ src/app/components/Intro.tsx
@@ -1,11 +1,12 @@
+import Link from "next/link";
 
 export default function Intro() {
     return (
         <>
-            <h2 className={'text-2xl mb-5'}>Hi, I'm Swam Htet.</h2>
+            <h2 className={'text-2xl mb-5'}>Hi, Im Swam Htet.</h2>
             <p className={'text-base indent-16'}>
                 Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the
-                industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and
+                industrys standard dummy text ever since the 1500s, when an unknown printer took a galley of type and
                 scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap
                 into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the
                 release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing
@@ -13,26 +14,26 @@ export default function Intro() {
             </p>
 
 
-            <a href={"https://youtube.com"}
+            <Link href={"https://youtube.com"}
                className="text-white bg-gray-700 mt-4 hover:bg-gray-800 focus:ring-4 focus:outline-none focus:ring-gray-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center inline-flex items-center mr-2">
-                <svg
-                    className="w-4 h-4 mr-4 text-white-800"
-                    aria-hidden="true"
-                    xmlns="http://www.w3.org/2000/svg"
-                    fill="none"
-                    viewBox="0 0 16 18"
-                >
-                    <path
-                        stroke="currentColor"
-                        strokeLinecap="round"
-                        strokeLinejoin="round"
-                        strokeWidth={2}
-                        d="M8 1v11m0 0 4-4m-4 4L4 8m11 4v3a2 2 0 0 1-2 2H3a2 2 0 0 1-2-2v-3"
-                    />
-                </svg>
+                {/*<svg*/}
+                {/*    className="w-4 h-4 mr-4 text-white-800"*/}
+                {/*    aria-hidden="true"*/}
+                {/*    xmlns="http://www.w3.org/2000/svg"*/}
+                {/*    fill="none"*/}
+                {/*    viewBox="0 0 16 18"*/}
+                {/*>*/}
+                {/*    <path*/}
+                {/*        stroke="currentColor"*/}
+                {/*        strokeLinecap="round"*/}
+                {/*        strokeLinejoin="round"*/}
+                {/*        strokeWidth={2}*/}
+                {/*        d="M8 1v11m0 0 4-4m-4 4L4 8m11 4v3a2 2 0 0 1-2 2H3a2 2 0 0 1-2-2v-3"*/}
+                {/*    />*/}
+                {/*</svg>*/}
 
                 Download CV
-            </a>
+            </Link>
 
 
         </>
